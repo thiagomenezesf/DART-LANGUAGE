dfd7b3a6648fdc5597f15e38bbfaee66f164eabc
Thiago Menezes Francisco <bv3032299@ad.sbv.ifsp.edu.br>
2026-05-06 11:55:12 -0300

    adicionar o arquivo 3

---
+++ ex3.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+bool VerificaEhPar(int a) {
+  bool ehPar = true;
+  
+   if(a % 2 != 0) {
+    ehPar = false;
+     
+     return ehPar;
+   } else {
+     return ehPar;
+   }
+  
+}
+
+void main() {
+  int n;
+  
+  String? input = stdin.readLineSync();
+  int? idade = int.tryParse(input);
+}
